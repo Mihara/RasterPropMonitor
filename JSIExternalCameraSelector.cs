0004f270abb7a92c4400210f74d4d529929c65c8
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-11-15 03:12:51 +0400

    Another support module.

---
+++ JSIExternalCameraSelector.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+namespace JSI
+{
+	public class JSIExternalCameraSelector: PartModule
+	{
+		// Actual configuration parameters.
+		[KSPField]
+		public string cameraContainer;
+		[KSPField]
+		public string cameraIDPrefix = "ExtCam";
+		[KSPField]
+		public int maximum = 8;
+		// Internal data storage.
+		[KSPField(isPersistant = true)]
+		public int current = 1;
+		// Fields to handle right-click GUI.
+		[KSPField(guiActive = true, guiName = "Camera ID: ")]
+		public string visibleCameraName;
+
+		[KSPEvent(guiActive = true, guiName = "ID+")]
+		public void IdPlus()
+		{
+			current++;
+			if (current > maximum)
+				current = 1;
+			updateName();
+		}
+
+		[KSPEvent(guiActive = true, guiName = "ID-")]
+		public void IdMinus()
+		{
+			current--;
+			if (current <= 0)
+				current = maximum;
+			updateName();
+		}
+
+		private void updateName()
+		{
+			Transform containingTransform = part.FindModelTransform(cameraContainer);
+			Transform actualCamera = null;
+			foreach (Transform thatTransform in containingTransform.gameObject.GetComponentsInChildren<Transform>()) {
+				if (containingTransform != thatTransform) {
+					actualCamera = thatTransform;
+					break;
+				}
+			}
+			visibleCameraName = actualCamera.name = cameraIDPrefix + current;
+		}
+
+		public override void OnStart(PartModule.StartState state)
+		{
+			updateName();
+		}
+	}
+}
+
