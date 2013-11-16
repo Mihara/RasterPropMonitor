e6663f4add93c952ee117ef6ac4be05f99dce71c
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-11-17 01:18:19 +0400

    Somewhat smoother handling of handlers.

--- MonitorPage.cs
+++ MonitorPage.cs
@@ -47,27 +47,10 @@ namespace JSI
 			ourMonitor = thatMonitor;
 			pageNumber = idNum;
 			isMutable = false;
-			if (!node.HasValue("text") && !node.HasValue("background") && !node.HasNode("PAGEHANDLER") && !node.HasNode("BACKGROUNDHANDLER") && !node.HasValue("button"))
-				throw new ArgumentException("A page needs to have either text, a background or a button.");
+			if (!node.HasData)
+				throw new ArgumentException("Empty page?");
 
 			isDefault |= node.HasValue("default");
-
-			if (node.HasValue("text")) {
-				string pageDefinition = node.GetValue("text");
-
-				try {
-					Text = String.Join(Environment.NewLine, File.ReadAllLines(KSPUtil.ApplicationRootPath + "GameData/" + pageDefinition, System.Text.Encoding.UTF8));
-					if (Text.IndexOf("$&$", StringComparison.Ordinal) != -1)
-						isMutable = true;
-				} catch (FileNotFoundException e) {
-					// There's no file.
-					Text = e.Message;
-				}
-				if (pageDefinition.IndexOf("$&$", StringComparison.Ordinal) != -1)
-					isMutable = true;
-				// But regardless of whether we found a page handler, it won't matter if we populate the page data or not.
-				Text = pageDefinition.Replace("<=", "{").Replace("=>", "}").Replace("$$$", Environment.NewLine);
-			}
 
 			if (node.HasValue("button")) {
 				SmarterButton.CreateButton(thatMonitor.internalProp, node.GetValue("button"), ButtonClick);
@@ -75,74 +58,92 @@ namespace JSI
 
 
 			if (node.HasNode("PAGEHANDLER")) {
-				ConfigNode pageHandlerNode = node.GetNode("PAGEHANDLER");
-				if (pageHandlerNode.HasValue("name") && pageHandlerNode.HasValue("method")) {
-					string moduleName = pageHandlerNode.GetValue("name");
-					string methodName = pageHandlerNode.GetValue("method");
+				InternalModule handlerModule;
+				MethodInfo handlerMethod = InstantiateHandler(node.GetNode("PAGEHANDLER"), out handlerModule);
+				if (handlerMethod != null && handlerModule != null) {
+					pageHandler = (Func<string>)Delegate.CreateDelegate(typeof(Func<string>), handlerModule, handlerMethod);
+					isMutable = true;
+				}
+			} else {
+				if (node.HasValue("text")) {
+					string pageDefinition = node.GetValue("text");
 
-					ConfigNode handlerConfiguration = new ConfigNode("MODULE");
-					pageHandlerNode.CopyTo(handlerConfiguration);
-					InternalModule thatModule = thatMonitor.internalProp.AddModule(handlerConfiguration);
-
-					foreach (MethodInfo m in thatModule.GetType().GetMethods()) {
-						if (m.Name == methodName) {
-							// We'll assume whoever wrote it is not being an idiot today.
-							pageHandler = (Func<string>)Delegate.CreateDelegate(typeof(Func<string>), thatModule, m);
+					try {
+						Text = String.Join(Environment.NewLine, File.ReadAllLines(KSPUtil.ApplicationRootPath + "GameData/" + pageDefinition, System.Text.Encoding.UTF8));
+						if (Text.IndexOf("$&$", StringComparison.Ordinal) != -1)
 							isMutable = true;
-							break;
-						}
+					} catch (FileNotFoundException e) {
+						// There's no file.
+						Debug.Log("There's no file named " + e.Message + ", assuming direct definition.");
+						if (pageDefinition.IndexOf("$&$", StringComparison.Ordinal) != -1)
+							isMutable = true;
+						Text = pageDefinition.Replace("<=", "{").Replace("=>", "}").Replace("$$$", Environment.NewLine);
 					}
 				}
 			}
+
 
 			background = BackgroundType.None;
 			if (node.HasNode("BACKGROUNDHANDLER")) {
-				// I hate copypaste, but I'll think of how to handle multiple return types from this mess a bit later.
-				ConfigNode backgroundHandlerNode = node.GetNode("BACKGROUNDHANDLER");
-				if (backgroundHandlerNode.HasValue("name") && backgroundHandlerNode.HasValue("method")) {
-					string moduleName = backgroundHandlerNode.GetValue("name");
-					string methodName = backgroundHandlerNode.GetValue("method");
-
-					ConfigNode handlerConfiguration = new ConfigNode("MODULE");
-					backgroundHandlerNode.CopyTo(handlerConfiguration);
-					InternalModule thatModule = thatMonitor.internalProp.AddModule(handlerConfiguration);
-
-					foreach (MethodInfo m in thatModule.GetType().GetMethods()) {
-						if (m.Name == methodName) {
-							backgroundHandler = (Func<RenderTexture,bool>)Delegate.CreateDelegate(typeof(Func<RenderTexture,bool>), thatModule, m);
-							isMutable = true;
-							break;
-						}
-					}
-
+				InternalModule handlerModule;
+				MethodInfo handlerMethod = InstantiateHandler(node.GetNode("BACKGROUNDHANDLER"), out handlerModule);
+				if (handlerMethod != null && handlerModule != null) {
+					backgroundHandler = (Func<RenderTexture,bool>)Delegate.CreateDelegate(typeof(Func<RenderTexture,bool>), handlerModule, handlerMethod);
+					isMutable = true;
+					background = BackgroundType.Handler;
 				}
-				background = BackgroundType.Handler;
-			} else if (node.HasValue("background")) {
-				switch (node.GetValue("background")) {
-					case "camera":
-						if (node.HasValue("cameraTransform")) {
-							isMutable = true;
-							background = BackgroundType.Camera;
-							camera = node.GetValue("cameraTransform");
-							if (node.HasValue("fov")) {
-								float fov;
-								cameraFOV = float.TryParse(node.GetValue("fov"), out fov) ? fov : defaultFOV;
-							} else
-								cameraFOV = defaultFOV;
+			} else {
+				if (node.HasValue("background")) {
+					switch (node.GetValue("background")) {
+						case "camera":
+							if (node.HasValue("cameraTransform")) {
+								isMutable = true;
+								background = BackgroundType.Camera;
+								camera = node.GetValue("cameraTransform");
+								if (node.HasValue("fov")) {
+									float fov;
+									cameraFOV = float.TryParse(node.GetValue("fov"), out fov) ? fov : defaultFOV;
+								} else
+									cameraFOV = defaultFOV;
 							
-						}
-						break;
-					case "texture":
-						if (node.HasValue("textureURL")) {
-							string textureURL = node.GetValue("textureURL");
-							if (GameDatabase.Instance.ExistsTexture(textureURL)) {
-								backgroundTexture = GameDatabase.Instance.GetTexture(textureURL, false);
-								background = BackgroundType.Texture;
 							}
-						}
-						break;
+							break;
+						case "texture":
+							if (node.HasValue("textureURL")) {
+								string textureURL = node.GetValue("textureURL");
+								if (GameDatabase.Instance.ExistsTexture(textureURL)) {
+									backgroundTexture = GameDatabase.Instance.GetTexture(textureURL, false);
+									background = BackgroundType.Texture;
+								}
+							}
+							break;
+					}
 				}
 			}
+		}
+
+		private MethodInfo InstantiateHandler(ConfigNode node, out InternalModule moduleInstance)
+		{
+			moduleInstance = null;
+			if (node.HasValue("name") && node.HasValue("method")) {
+				string moduleName = node.GetValue("name");
+				string methodName = node.GetValue("method");
+
+				ConfigNode handlerConfiguration = new ConfigNode("MODULE");
+				node.CopyTo(handlerConfiguration);
+				InternalModule thatModule = ourMonitor.internalProp.AddModule(handlerConfiguration);
+
+				if (thatModule == null)
+					return null;
+				moduleInstance = thatModule;
+				foreach (MethodInfo m in thatModule.GetType().GetMethods()) {
+					if (m.Name == methodName) {
+						return m;
+					}
+				}
+
+			}
+			return null;
 		}
 
 		public bool RenderBackground(RenderTexture screen)
