965a8a88f52f17325f3179db203f731472298f7a
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-11-19 01:23:32 +0400

    Le argh.

--- Auxiliary modules/JSIPrimaryFlightDisplay.cs
+++ Auxiliary modules/JSIPrimaryFlightDisplay.cs
@@ -5,7 +5,11 @@ namespace JSI
 	public class JSIPrimaryFlightDisplay: InternalModule
 	{
 		[KSPField]
+		public int drawingLayer = 17;
+		[KSPField]
 		public string horizonTexture;
+		[KSPField]
+		public string navBallModel;
 		[KSPField]
 		public string staticOverlay;
 		[KSPField]
@@ -13,14 +17,12 @@ namespace JSI
 		[KSPField]
 		public float ballAspect = 1.35f;
 
-		private Material horizonMaterial;
+		private Texture2D horizonTex;
 		private Material overlayMaterial;
 		private Material headingMaterial;
 
 		private GameObject navBall;
 		private Camera ballCamera;
-
-		private const int ballLayer = 17;
 
 		public bool RenderPFD(RenderTexture screen)
 		{
@@ -35,100 +37,24 @@ namespace JSI
 			Quaternion rotationVesselSurface = Quaternion.Inverse(Quaternion.Euler(90, 0, 0) * Quaternion.Inverse(vessel.GetTransform().rotation) * rotationSurface);
 
 			ballCamera.targetTexture = screen;
+			navBall.SetActive(true);
 			navBall.renderer.enabled = true;
-			navBall.transform.rotation = rotationVesselSurface;
+
+			//navBall.transform.rotation = rotationVesselSurface;
+			//navBall.transform.localRotation = FlightGlobals.ship_orientation;
+
 			ballCamera.Render();
 			navBall.renderer.enabled = false;
+			navBall.SetActive(false);
 
-			GL.PushMatrix();
-			GL.LoadOrtho();
-
-			//DrawHorizon(rotationVesselSurface.eulerAngles.z, rotationVesselSurface.eulerAngles.x);
-
-			DrawHeadingBar(rotationVesselSurface.eulerAngles.y);
-			DrawOverlay();
-
-			GL.PopMatrix();
+			//GL.PushMatrix();
+			//GL.LoadOrtho();
+			//GL.PopMatrix();
 			return true;
 
 		}
 
 
-		// It all went low-level and downhill from there.
-
-		private void DrawHeadingBar(float heading)
-		{
-
-			float xShift = Mathf.Lerp(0, 1, Mathf.InverseLerp(0, 360, (float)JUtil.ClampDegrees360(heading)));
-			const float span = 0.25f;
-			xShift -= span / 2f;
-			Vector3 bottomLeft = new Vector3(0.2f, 0.8f);
-			Vector3 topLeft = new Vector3(0.2f, 0.9f);
-			Vector3 bottomRight = new Vector3(0.8f, 0.8f);
-			Vector3 topRight = new Vector3(0.8f, 0.9f);
-
-			headingMaterial.SetPass(0);
-			GL.Begin(GL.QUADS);
-			GL.Color(Color.white);
-			// Examples seem to do it clockwise.
-			GL.TexCoord2(0 + xShift, 0);
-			GL.Vertex(bottomLeft);
-			GL.TexCoord2(0 + xShift, 1);
-			GL.Vertex(topLeft);
-			GL.TexCoord2(span + xShift, 1);
-			GL.Vertex(topRight);
-			GL.TexCoord2(span + xShift, 0);
-			GL.Vertex(bottomRight);
-			GL.End();
-		}
-
-		private void DrawOverlay()
-		{
-			overlayMaterial.SetPass(0);
-			DrawQuad(new Vector3(0.25f, 0.25f), new Vector3(0.25f, 0.75f), new Vector3(0.75f, 0.75f), new Vector3(0.75f, 0.25f), 0, 0);
-		}
-
-		private static void DrawQuad(Vector3 bottomLeft, Vector3 topLeft, Vector3 topRight, Vector3 bottomRight, float xShift, float yShift)
-		{
-			GL.Begin(GL.QUADS);
-			GL.Color(Color.white);
-			// Examples seem to do it clockwise.
-			GL.TexCoord2(0 + xShift, 0 + yShift);
-			GL.Vertex(bottomLeft);
-			GL.TexCoord2(0 + xShift, 1 + yShift);
-			GL.Vertex(topLeft);
-			GL.TexCoord2(1 + xShift, 1 + yShift);
-			GL.Vertex(topRight);
-			GL.TexCoord2(1 + xShift, 0 + yShift);
-			GL.Vertex(bottomRight);
-			GL.End();
-		}
-
-		private void DrawHorizon(float rollAngle, float pitchAngle)
-		{
-			Vector3[] corners = {
-				new Vector3(-0.5f, -0.5f),
-				new Vector3(-0.5f, 1.5f),
-				new Vector3(1.5f, 1.5f),
-				new Vector3(1.5f, -0.5f)
-			};
-
-			Vector3 center = new Vector3(0.5f, 0.5f);
-			Quaternion angleQuat = Quaternion.Euler(0, 0, -rollAngle);
-			for (int i = 0; i < corners.Length; i++)
-				corners[i] = RotateAroundPoint(corners[i], center, angleQuat);
-
-			horizonMaterial.SetPass(0);
-			DrawQuad(corners[0], corners[1], corners[2], corners[3],
-				0,
-				Mathf.Lerp(1, 0, Mathf.InverseLerp(0, 360, pitchAngle))
-			);
-		}
-
-		private static Vector3 RotateAroundPoint(Vector3 point, Vector3 pivot, Quaternion angle)
-		{
-			return angle * (point - pivot) + pivot;
-		}
 
 		public override void OnUpdate()
 		{
@@ -136,39 +62,37 @@ namespace JSI
 
 		public void Start()
 		{
-			Shader unlit = Shader.Find("Unlit/Transparent");
-			horizonMaterial = new Material(unlit);
-			horizonMaterial.SetTexture("_MainTex", GameDatabase.Instance.GetTexture(horizonTexture, false));
+			Shader unlit = Shader.Find("KSP/Alpha/Unlit Transparent");
 			overlayMaterial = new Material(unlit);
 			overlayMaterial.SetTexture("_MainTex", GameDatabase.Instance.GetTexture(staticOverlay, false));
 			headingMaterial = new Material(unlit);
 			headingMaterial.SetTexture("_MainTex", GameDatabase.Instance.GetTexture(headingBar, false));
 
-			navBall = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			horizonTex = GameDatabase.Instance.GetTexture(horizonTexture, false);
+			navBall = GameDatabase.Instance.GetModel(navBallModel);
+			navBall.SetActive(false);
 			navBall.name = "RPMNB"+navBall.GetInstanceID();
-			navBall.layer = ballLayer;
-			navBall.transform.position = Vector3.zero;
+			navBall.layer = drawingLayer;
+			navBall.transform.position = new Vector3(0,0,0);
+			navBall.transform.rotation = Quaternion.identity;
 			navBall.transform.localRotation = Quaternion.identity;
-			navBall.transform.localScale = new Vector3(1f, 1f, 1f);
-			navBall.renderer.material = horizonMaterial;
+			navBall.renderer.material.SetTexture("_MainTex",horizonTex);
 			navBall.renderer.enabled = false;
-			//horizonMaterial.SetTextureOffset("_MainTex",new Vector2(0.5f,0));
-			horizonMaterial.SetTextureScale("_MainTex",new Vector2(-1f,1f));
-			navBall.collider.enabled = false;
 
 
 			GameObject cameraBody = new GameObject();
 			cameraBody.name = "RPMPFD"+cameraBody.GetInstanceID();
-			cameraBody.layer = ballLayer;
+			cameraBody.layer = drawingLayer;
 			ballCamera = cameraBody.AddComponent<Camera>();
 			ballCamera.enabled = false;
 			ballCamera.orthographic = true;
 			ballCamera.aspect = ballAspect;
 			ballCamera.orthographicSize = 0.7f;
-			ballCamera.cullingMask = 1 << ballLayer;
+			ballCamera.cullingMask = 1 << drawingLayer;
 			ballCamera.transform.position = new Vector3(2, 0, 0);
-			ballCamera.transform.LookAt(navBall.transform, new Vector3(0,1,0));
+			ballCamera.transform.LookAt(Vector3.zero);
 		}
+
 	}
 }
 
