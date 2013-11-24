7882e026a4e3e51284b87dd11cbb696e8aa6ab82
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-11-25 00:23:46 +0400

    Pretty lights!

--- Auxiliary modules/JSIExternalCameraSelector.cs
+++ Auxiliary modules/JSIExternalCameraSelector.cs
@@ -17,6 +17,12 @@ namespace JSI
 		// Fields to handle right-click GUI.
 		[KSPField(guiActive = true, guiName = "Camera ID: ")]
 		public string visibleCameraName;
+		private GameObject lightCone;
+		private LineRenderer lightConeRenderer;
+		private static readonly Material lightConeMaterial = new Material(Shader.Find("Particles/Additive"));
+		private Transform actualCamera;
+		private const float endSpan = 15f;
+		private const float fovAngle = 60f;
 
 		[KSPEvent(guiActive = true, guiName = "ID+")]
 		public void IdPlus()
@@ -39,7 +45,6 @@ namespace JSI
 		private void UpdateName()
 		{
 			Transform containingTransform = part.FindModelTransform(cameraContainer);
-			Transform actualCamera = null;
 			foreach (Transform thatTransform in containingTransform.gameObject.GetComponentsInChildren<Transform>()) {
 				if (containingTransform != thatTransform) {
 					actualCamera = thatTransform;
@@ -59,6 +64,55 @@ namespace JSI
 		public override void OnStart(PartModule.StartState state)
 		{
 			UpdateName();
+			if (state == StartState.Editor) {
+				CreateLightCone();
+				part.OnEditorAttach += new Callback(DestroyLightCone);
+				part.OnEditorDetach += new Callback(CreateLightCone);
+			} else {
+				DestroyLightCone();
+			}
+		}
+
+		private void CreateLightCone()
+		{
+			RenderingManager.AddToPostDrawQueue(0, DrawLightCone);
+			if (lightConeRenderer == null) {
+				lightCone = new GameObject();
+				lightConeRenderer = lightCone.AddComponent<LineRenderer>();
+				lightConeRenderer.useWorldSpace = true;
+				lightConeRenderer.material = lightConeMaterial;
+				lightConeRenderer.SetColors(new Color(0f, 0f, 1f, 1f), new Color(0f, 0f, 1f, 0f));
+				lightConeRenderer.SetWidth(0.054f, endSpan);
+				lightConeRenderer.SetVertexCount(2);
+				lightConeRenderer.castShadows = false;
+				lightConeRenderer.receiveShadows = false;
+				lightConeRenderer.SetPosition(0, Vector3.zero);
+				lightConeRenderer.SetPosition(1, Vector3.zero);
+			}
+		}
+
+		private void DestroyLightCone()
+		{
+			RenderingManager.RemoveFromPostDrawQueue(0, DrawLightCone);
+			if (lightConeRenderer != null) {
+				Destroy(lightConeRenderer);
+				lightConeRenderer = null;
+				Destroy(lightCone);
+				lightCone = null;
+			}
+		}
+
+		public void DrawLightCone()
+		{
+			if (!HighLogic.LoadedSceneIsEditor) {
+				DestroyLightCone();
+			}
+			if (lightConeRenderer != null) {
+				Vector3 origin = actualCamera.transform.TransformPoint(Vector3.zero);
+				Vector3 direction = actualCamera.transform.TransformDirection(Vector3.forward);
+				lightConeRenderer.SetPosition(0, origin);
+				lightConeRenderer.SetPosition(1, origin + direction * (endSpan / 2 / Mathf.Tan(Mathf.Deg2Rad * fovAngle / 2)));
+			}
 		}
 	}
 }
