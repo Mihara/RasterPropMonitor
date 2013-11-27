67a5ce40de68fb98f55da29cd985f5a3ba2e1788
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-11-28 00:03:20 +0400

    And one more helper module

---
+++ RasterPropMonitor/Auxiliary modules/JSISetInternalCameraFOV.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace JSI
+{
+	public class JSISetInternalCameraFOV: InternalModule
+	{
+		[KSPField]
+		public float fov = 60f;
+		private CameraManager.CameraMode oldCameraMode;
+
+		public override void OnUpdate()
+		{
+			if (!HighLogic.LoadedSceneIsFlight || vessel != FlightGlobals.ActiveVessel)
+				return;
+
+			if (!(CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.IVA ||
+			    CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.Internal))
+				return;
+			if (CameraManager.Instance.currentCameraMode != oldCameraMode && InternalCamera.Instance.isActive) {
+				InternalCamera.Instance.SetFOV(fov);
+				oldCameraMode = CameraManager.Instance.currentCameraMode;
+			}
+		}
+	}
+}
+
