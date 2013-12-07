56eabb9bd01cfddb838f45b4e88d865d8dad1a0f
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-12-07 12:21:28 +0400

    That was actually too aggressive.

--- RasterPropMonitor/Auxiliary modules/InternalCameraTargetHelper.cs
+++ RasterPropMonitor/Auxiliary modules/InternalCameraTargetHelper.cs
@@ -2,30 +2,26 @@ namespace JSI
 {
 	public class InternalCameraTargetHelper: InternalModule
 	{
-		ITargetable target;
+		private ITargetable target;
+		private CameraManager.CameraMode previousCameraMode;
 
 		public override void OnUpdate()
 		{
-			if (!HighLogic.LoadedSceneIsFlight ||
-			    vessel != FlightGlobals.ActiveVessel)
+			if (!HighLogic.LoadedSceneIsFlight || vessel != FlightGlobals.ActiveVessel)
 				return;
 
 			if (!(CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.IVA ||
 			    CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.Internal))
 				return;
 
-			if (CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.IVA) {
-				ITargetable newTarget = FlightGlobals.fetch.VesselTarget;
-				if (newTarget == null && target != null)
+			if (CameraManager.Instance.currentCameraMode != previousCameraMode) {
+				if (FlightGlobals.fetch.VesselTarget == null && target != null)
 					FlightGlobals.fetch.SetVesselTarget(target);
-				else
-					target = newTarget;
+
 			}
-			if (CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.Internal &&
-			    FlightGlobals.fetch.VesselTarget == null && target != null)
-				FlightGlobals.fetch.SetVesselTarget(target);
 
-
+			target = FlightGlobals.fetch.VesselTarget;
+			previousCameraMode = CameraManager.Instance.currentCameraMode;
 		}
 	}
 }
