ca05078991d1f38a4f34192d4a11a19192bc77cb
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-12-12 22:59:06 +0400

    More failure-resistant now.
    Now it dies with an error message instead of producing endless log spam.

--- RasterPropMonitor/Core/ButtonHandler.cs
+++ RasterPropMonitor/Core/ButtonHandler.cs
@@ -21,41 +21,49 @@ namespace JSI
 				if (listCounter >= pageReferences.Count)
 					listCounter = 0;
 			}
-			if (handlerID != null) {
+			if (handlerID != null)
 				handlerID(id);
-			}
 			if (handler != null)
 				handler();
 		}
 
 		private static SmarterButton AttachBehaviour(InternalProp thatProp, string buttonName)
 		{
-			GameObject buttonObject = thatProp.FindModelTransform(buttonName).gameObject;
-			if (buttonObject == (UnityEngine.Object)null) {
-				Debug.LogError("Button transform name not found, expect errors.");
-				return null;
-			} 
-			SmarterButton thatComponent = buttonObject.GetComponent<SmarterButton>() ?? buttonObject.AddComponent<SmarterButton>();
-			return thatComponent;
+			try {
+				GameObject buttonObject = thatProp.FindModelTransform(buttonName).gameObject;
+				SmarterButton thatComponent = buttonObject.GetComponent<SmarterButton>() ?? buttonObject.AddComponent<SmarterButton>();
+				return thatComponent;
+			} catch {
+				Debug.LogError(string.Format(
+					"ERROR: Could not register a button on transform named '{0}' in prop named '{1}'. Check your configuration.",
+					buttonName, thatProp.propName));
+			}
+			return null;
 		}
 
 		public static void CreateButton(InternalProp thatProp, string buttonName, MonitorPage thatPage, Action<MonitorPage> handlerFunction)
 		{
-			SmarterButton buttonBehaviour = AttachBehaviour(thatProp, buttonName);
+			SmarterButton buttonBehaviour;
+			if ((buttonBehaviour = AttachBehaviour(thatProp, buttonName)) == null)
+				return;
 			buttonBehaviour.pageSelectionHandlerFunction = handlerFunction;
 			buttonBehaviour.pageReferences.Add(thatPage);
 		}
 
 		public static void CreateButton(InternalProp thatProp, string buttonName, int id, Action<int> handlerFunction)
 		{
-			SmarterButton buttonBehaviour = AttachBehaviour(thatProp, buttonName);
+			SmarterButton buttonBehaviour;
+			if ((buttonBehaviour = AttachBehaviour(thatProp, buttonName)) == null)
+				return;
 			buttonBehaviour.id = id;
 			buttonBehaviour.handlerID = handlerFunction;
 		}
 
 		public static void CreateButton(InternalProp thatProp, string buttonName, Action handlerFunction)
 		{
-			SmarterButton buttonBehaviour = AttachBehaviour(thatProp, buttonName);
+			SmarterButton buttonBehaviour;
+			if ((buttonBehaviour = AttachBehaviour(thatProp, buttonName)) == null)
+				return;
 			buttonBehaviour.handler = handlerFunction;
 		}
 	}
