84f7ab8f7790d8749b9ea8dcf9e8860d22ac39a1
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-12-12 11:36:39 +0400

    Well, looks like I can.
    Turns out that GetMasterMechJeb method gets cached.

--- MechJebRPM/MechJebRPM.cs
+++ MechJebRPM/MechJebRPM.cs
@@ -4,16 +4,14 @@ namespace MechJebRPM
 {
 	public class MechJebRPM: InternalModule
 	{
-
 		MechJebCore activeJeb;
 		MechJebModuleSmartASS activeSmartass;
-
 		bool pageActiveState;
-
 		// Analysis disable UnusedParameter
 		public string ShowMenu(int width, int height)
 		// Analysis restore UnusedParameter
 		{
+			UpdateJebReferences();
 			if (activeSmartass != null) {
 				return activeSmartass.mode.ToString();
 			}
@@ -25,12 +23,19 @@ namespace MechJebRPM
 			pageActiveState = active;
 		}
 
-		public void Start()
+		private void UpdateJebReferences()
 		{
 			activeJeb = vessel.GetMasterMechJeb();
 			if (activeJeb != null) {
 				activeSmartass = activeJeb.GetComputerModule<MechJebModuleSmartASS>();
+			} else {
+				activeSmartass = null;
 			}
+		}
+
+		public void Start()
+		{
+			UpdateJebReferences();
 		}
 	}
 }
