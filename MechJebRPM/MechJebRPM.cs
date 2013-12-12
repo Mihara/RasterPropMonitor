45296aa4a437f0865c073bbd2328aef6276730a3
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-12-12 11:32:54 +0400

    Ok, that approach works.
    If I can quickly find out if that mechjeb unit I'm pointing at is still
    the master, I'm all set.

--- MechJebRPM/MechJebRPM.cs
+++ MechJebRPM/MechJebRPM.cs
@@ -1,8 +1,37 @@
+using MuMech;
 
 namespace MechJebRPM
 {
-	public class MechJebRPM
+	public class MechJebRPM: InternalModule
 	{
+
+		MechJebCore activeJeb;
+		MechJebModuleSmartASS activeSmartass;
+
+		bool pageActiveState;
+
+		// Analysis disable UnusedParameter
+		public string ShowMenu(int width, int height)
+		// Analysis restore UnusedParameter
+		{
+			if (activeSmartass != null) {
+				return activeSmartass.mode.ToString();
+			}
+			return "Not found.";
+		}
+		// Analysis disable once UnusedParameter
+		public void PageActive(bool active, int pageNumber)
+		{
+			pageActiveState = active;
+		}
+
+		public void Start()
+		{
+			activeJeb = vessel.GetMasterMechJeb();
+			if (activeJeb != null) {
+				activeSmartass = activeJeb.GetComputerModule<MechJebModuleSmartASS>();
+			}
+		}
 	}
 }
 
