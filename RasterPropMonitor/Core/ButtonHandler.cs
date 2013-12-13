d83d0ac7b707b229a80c7a889f3acfb5fe7d8a67
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-12-13 18:58:30 +0400

    That, actually, is what I wanted it to do from the start.

--- RasterPropMonitor/Core/ButtonHandler.cs
+++ RasterPropMonitor/Core/ButtonHandler.cs
@@ -6,34 +6,55 @@ namespace JSI
 {
 	public class SmarterButton: MonoBehaviour
 	{
-		private readonly List<handlerID> clickHandlersID = new List<handlerID>();
-		private readonly List<handlerID> releaseHandlersID = new List<handlerID>();
+		private readonly List<HandlerID> clickHandlersID = new List<HandlerID>();
+		private readonly List<HandlerID> releaseHandlersID = new List<HandlerID>();
 		private readonly List<Action> clickHandlers = new List<Action>();
 		private readonly List<Action> releaseHandlers = new List<Action>();
-		private readonly List<pageTrigger> pageReferences = new List<pageTrigger>();
-		private int listCounter;
+		private readonly List<PageTriggerSet> pageTriggers = new List<PageTriggerSet>();
 
-		private struct handlerID
+		private struct HandlerID
 		{
 			public Action<int> function;
 			public int idValue;
 		}
 
-		private struct pageTrigger
+		private class PageTriggerSet
 		{
-			public Action<MonitorPage> selector;
-			public MonitorPage page;
+			private int counter;
+			private readonly Action<MonitorPage> selector;
+			private readonly List<MonitorPage> pages = new List<MonitorPage>();
+
+			public PageTriggerSet(Action<MonitorPage> function, MonitorPage page)
+			{
+				selector = function;
+				pages.Add(page);
+			}
+
+			public bool Add(Action<MonitorPage> function, MonitorPage page)
+			{
+				if (function == selector) {
+					pages.Add(page);
+					return true;
+				}
+				return false;
+			}
+
+			public void ShowNext()
+			{
+				if (pages.Count > 0) {
+					selector(pages[counter]);
+					counter++;
+					if (counter >= pages.Count)
+						counter = 0;
+				}
+			}
 		}
 
 		public void OnMouseDown()
 		{
-			if (pageReferences.Count > 0) {
-				pageReferences[listCounter].selector(pageReferences[listCounter].page);
-				listCounter++;
-				if (listCounter >= pageReferences.Count)
-					listCounter = 0;
-			}
-			foreach (handlerID consumer in clickHandlersID) {
+			foreach (PageTriggerSet monitor in pageTriggers)
+				monitor.ShowNext();
+			foreach (HandlerID consumer in clickHandlersID) {
 				consumer.function(consumer.idValue);
 			}
 			foreach (Action clickHandler in clickHandlers) {
@@ -43,7 +64,7 @@ namespace JSI
 
 		public void OnMouseUp()
 		{
-			foreach (handlerID consumer in releaseHandlersID) {
+			foreach (HandlerID consumer in releaseHandlersID) {
 				consumer.function(consumer.idValue);
 			}
 			foreach (Action releaseHandler in releaseHandlers) {
@@ -84,10 +105,11 @@ namespace JSI
 			SmarterButton buttonBehaviour;
 			if ((buttonBehaviour = AttachBehaviour(thatProp, buttonName)) == null)
 				return;
-			buttonBehaviour.pageReferences.Add(new pageTrigger {
-				selector = handlerFunction,
-				page = thatPage
-			});
+			foreach (PageTriggerSet pageset in buttonBehaviour.pageTriggers) {
+				if (pageset.Add(handlerFunction, thatPage))
+					return;
+			}
+			buttonBehaviour.pageTriggers.Add(new PageTriggerSet(handlerFunction, thatPage));
 		}
 
 		public static void CreateButton(InternalProp thatProp, string buttonName, int numericID, Action<int> clickHandlerFunction, Action<int> releaseHandlerFunction)
@@ -96,11 +118,11 @@ namespace JSI
 			if ((buttonBehaviour = AttachBehaviour(thatProp, buttonName)) == null)
 				return;
 
-			buttonBehaviour.clickHandlersID.Add(new handlerID {
+			buttonBehaviour.clickHandlersID.Add(new HandlerID {
 				function = clickHandlerFunction,
 				idValue = numericID
 			});
-			buttonBehaviour.releaseHandlersID.Add(new handlerID {
+			buttonBehaviour.releaseHandlersID.Add(new HandlerID {
 				function = releaseHandlerFunction,
 				idValue = numericID
 			});
