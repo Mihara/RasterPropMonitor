1976a09d6979946d59010d029a4e06a206cabb12
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-12-23 00:21:19 +0400

    I still think that's the best way. :)

--- RasterPropMonitor/Core/PropMonitorComputer.cs
+++ RasterPropMonitor/Core/PropMonitorComputer.cs
@@ -960,6 +960,11 @@ namespace JSI
 							return -1d;
 					}
 					return double.NaN;
+
+			// Compound variables which exist to stave off the need to parse logical and arithmetic expressions. :)
+				case "GROUNDPROXIMITYALARM":
+					// Returns 1 if vertical speed is negative, gear is not extended, and radar altitude is less than 50m.
+					return (speedVertical < 0 && !FlightGlobals.ActiveVessel.ActionGroups.groups[gearGroupNumber] && altitudeTrue < 50).GetHashCode();
 			// SCIENCE!!
 				case "SCIENCEDATA":
 					return totalDataAmount;
