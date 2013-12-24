c3ba50b0879a818be82f68f7fd790995e32475ce
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-12-25 00:38:53 +0400

    Keeper of the reference transform

--- RasterPropMonitor/Auxiliary modules/InternalCameraTargetHelper.cs
+++ RasterPropMonitor/Auxiliary modules/InternalCameraTargetHelper.cs
@@ -4,17 +4,30 @@ namespace JSI
 	{
 		private ITargetable target;
 		private bool needsRestoring;
+		private static Part referencePart;
+		private bool wasOutsideOnPreviousFrame = true;
 
 		public override void OnUpdate()
 		{
-			if (!JUtil.VesselIsInIVA(vessel))
+			if (!JUtil.VesselIsInIVA(vessel)) {
+				wasOutsideOnPreviousFrame = true;
 				return;
+			}
 
+			// Restoring target.
 			if (needsRestoring && target != null && FlightGlobals.fetch.VesselTarget == null) {
 				FlightGlobals.fetch.SetVesselTarget(target);
-			} else
+			} else {
 				target = FlightGlobals.fetch.VesselTarget;
+			}
 			needsRestoring = false;
+
+			// Restoring reference.
+			if (wasOutsideOnPreviousFrame && referencePart != null && referencePart.vessel == vessel) {
+				referencePart.MakeReferencePart();
+			}
+			referencePart = vessel.GetReferenceTransformPart();
+			wasOutsideOnPreviousFrame = false;
 		}
 
 		public void LateUpdate()
