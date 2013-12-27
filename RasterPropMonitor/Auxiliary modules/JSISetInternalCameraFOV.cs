ec1455797720791c8df761e346d51995ba6ab9d5
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-12-28 03:21:02 +0400

    Needed for correct use of JSIInternalEVAHatch

--- RasterPropMonitor/Auxiliary modules/JSISetInternalCameraFOV.cs
+++ RasterPropMonitor/Auxiliary modules/JSISetInternalCameraFOV.cs
@@ -5,12 +5,21 @@ namespace JSI
 	{
 		[KSPField]
 		public float fov = 60f;
+		[KSPField]
+		public float maxRot = 60f;
+		[KSPField]
+		public float maxPitch = 60f;
+		[KSPField]
+		public float minPitch = -30f;
 		private CameraManager.CameraMode oldCameraMode;
 
 		public override void OnUpdate()
 		{
 			if (JUtil.VesselIsInIVA(vessel) && CameraManager.Instance.currentCameraMode != oldCameraMode && InternalCamera.Instance.isActive) {
 				InternalCamera.Instance.SetFOV(fov);
+				InternalCamera.Instance.maxRot = maxRot;
+				InternalCamera.Instance.maxPitch = maxPitch;
+				InternalCamera.Instance.minPitch = minPitch;
 				oldCameraMode = CameraManager.Instance.currentCameraMode;
 			}
 		}
