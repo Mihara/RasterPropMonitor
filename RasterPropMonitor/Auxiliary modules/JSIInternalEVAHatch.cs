14a1d37e54693c5dde7ba26886817dcbe5f15495
Eugene Medvedev <rn3aoh.g@gmail.com>
2013-12-28 01:29:49 +0400

    Now let's see if it works.

--- RasterPropMonitor/Auxiliary modules/JSIInternalEVAHatch.cs
+++ RasterPropMonitor/Auxiliary modules/JSIInternalEVAHatch.cs
@@ -1,11 +1,37 @@
 using System;
+using UnityEngine;
 
 namespace JSI
 {
-	public class JSIInternalEVAHatch
+	public class JSIInternalEVAHatch: InternalModule
 	{
-		public JSIInternalEVAHatch()
+		[KSPField]
+		public string hatchTransform = string.Empty;
+
+		public void Start()
 		{
+			if (string.IsNullOrEmpty(hatchTransform)) {
+				JUtil.LogMessage(this, "Where's my transform?");
+				return;
+			}
+			if (internalProp == null) {
+				SmarterButton.CreateButton(null, hatchTransform, EVAClick, null, internalModel);
+			} else {
+				SmarterButton.CreateButton(internalProp, hatchTransform, EVAClick);
+			}
+		}
+
+		public void EVAClick()
+		{
+			// I wish there were a more sensible way to do it, but looks like there is no other way to get a pointer to it.
+			IVACamera currentCamera = (IVACamera)FindObjectOfType(typeof(IVACamera));
+			if (currentCamera == null)
+				return;
+			if (HighLogic.CurrentGame.Parameters.Flight.CanEVA) {
+				CameraManager.Instance.SetCameraFlight();
+				FlightEVA.SpawnEVA(currentCamera.kerbal);
+			}
+
 		}
 	}
 }
