ef6c570028fc90d0554cf74f6a35c083804f0365
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-01-24 18:23:14 +0400

    I declare thee 0.15-dev.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct ("RasterPropMonitor")]
-[assembly: AssemblyCopyright ("Copyright ©2013 by Mihara and other contributors, released under the terms of GNU GPLv3")]
+[assembly: AssemblyCopyright ("Copyright ©2013-2014 by Mihara and other contributors, released under the terms of GNU GPLv3")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("0.14.*")]
+[assembly: AssemblyVersion("0.15.*")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
