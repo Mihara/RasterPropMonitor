b0993998caefd669cca40b6ee9bfaee5fa545d6b
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-06-14 19:29:13 +0400

    Much better, still not quite it.
    Now I don't get why the hell do the RPM modules inside go missing.

--- RasterPropMonitor/Auxiliary modules/JSITransparentPod.cs
+++ RasterPropMonitor/Auxiliary modules/JSITransparentPod.cs
@@ -15,44 +15,23 @@ namespace JSI
 		[KSPField]
 		public string transparentShaderName = "Transparent/Specular";
 		[KSPField]
+		public string opaqueShaderName = string.Empty;
+		[KSPField]
 		public bool restoreShadersOnIVA = true;
 
 		// I would love to know what exactly possessed Squad to have the IVA space use it's own coordinate system.
 		// This rotation adjusts IVA space to match the 'real' space...
 		private Quaternion MagicalVoodooRotation = new Quaternion(0, 0.7f, -0.7f, 0);
 
+		private Part knownRootPart;
+		private Vessel lastActiveVessel;
 		private Quaternion originalRotation;
 		private Transform originalParent;
 		private Vector3 originalPosition;
 
-		private Shader transparentShader;
+		private Shader transparentShader, opaqueShader;
+		private bool hasOpaqueShader;
 		private readonly Dictionary<Transform,Shader> shadersBackup = new Dictionary<Transform, Shader>();
-
-		private bool shadersAreTransparent;
-
-		public override void OnAwake()
-		{
-			// Apply shaders to transforms on startup.
-			if (!string.IsNullOrEmpty(transparentTransforms)) {
-
-				transparentShader = Shader.Find(transparentShaderName);
-
-				foreach (string transformName in transparentTransforms.Split('|')) {
-					try {
-						Transform tr = part.FindModelTransform(transformName.Trim());
-						if (tr != null) {
-							// We both change the shader and backup the original shader so we can undo it later.
-							Shader backupShader = tr.renderer.material.shader;
-							tr.renderer.material.shader = transparentShader;
-							shadersBackup.Add(tr, backupShader);
-						}
-					} catch (Exception e) {
-						Debug.LogException(e, this);
-					}
-				}
-				shadersAreTransparent = true;
-			}
-		}
 
 		private static void SetCameraCullingMask(string cameraName, bool flag)
 		{
@@ -73,6 +52,31 @@ namespace JSI
 		{
 
 			JUtil.LogMessage(this, "Cleaning pod windows...");
+
+			// Apply shaders to transforms on startup.
+			if (!string.IsNullOrEmpty(transparentTransforms)) {
+
+				transparentShader = Shader.Find(transparentShaderName);
+
+				foreach (string transformName in transparentTransforms.Split('|')) {
+					try {
+						Transform tr = part.FindModelTransform(transformName.Trim());
+						if (tr != null) {
+							// We both change the shader and backup the original shader so we can undo it later.
+							Shader backupShader = tr.renderer.material.shader;
+							tr.renderer.material.shader = transparentShader;
+							shadersBackup.Add(tr, backupShader);
+						}
+					} catch (Exception e) {
+						Debug.LogException(e, this);
+					}
+				}
+			}
+
+			if (!string.IsNullOrEmpty(opaqueShaderName)) {
+				opaqueShader = Shader.Find(opaqueShaderName);
+				hasOpaqueShader = true;
+			}
 
 			// In Editor, the camera we want to change is called "Main Camera". In flight, the camera to change is
 			// "Camera 00", i.e. close range camera.
@@ -95,20 +99,64 @@ namespace JSI
 			// If we ended up with an existing internal model, rotate it now, so that it is shown correctly in the editor.
 			if (part.internalModel != null) {
 				if (state == StartState.Editor) {
+					// Just rotating the internal is sufficient in this case.
 					part.internalModel.transform.localRotation = MagicalVoodooRotation;
 				} else {
-					// Now the cruical bit. This is our first startup in a non-editor scene, so
-					// we record the parent and rotation of the InternalModel so we can swap it back in.
-					originalParent = part.internalModel.transform.parent;
-					originalPosition = part.internalModel.transform.localPosition;
-					originalRotation = part.internalModel.transform.localRotation;
-					// In case we're starting up because the ship just got unpacked, we also make sure to spawn the crew.
-					part.SpawnCrew();
+					// Else this is our first startup in flight scene, so we reset the IVA.
+					ResetIVA();
 				}
 			} else {
 				// Some error-proofing. I won't bother doing this every frame, because one error message
 				// should suffice, this module is not supposed to be attached to parts that have no internals in the first place.
 				JUtil.LogErrorMessage(this, "Wait, where's my internal model?");
+			}
+		}
+
+		private void SetShaders(bool state)
+		{
+			if (restoreShadersOnIVA) {
+				if (state) {
+					foreach (KeyValuePair<Transform,Shader> backup in shadersBackup) {
+						backup.Key.renderer.material.shader = transparentShader;
+					}
+				} else {
+					foreach (KeyValuePair<Transform,Shader> backup in shadersBackup) {
+						backup.Key.renderer.material.shader = hasOpaqueShader ? opaqueShader : backup.Value;
+					}
+				}
+			}
+		}
+
+		private void ResetIVA()
+		{
+
+			if (HighLogic.LoadedSceneIsFlight) {
+				JUtil.LogMessage(this, "Resetting IVA because vessel state changed...");
+
+				// Now the cruical bit.
+				// If the root part changed, we actually need to recreate the IVA forcibly even if it still exists.
+				if (vessel.rootPart != knownRootPart) {
+					part.CreateInternalModel();
+				}
+				// But otherwise the existing one will serve.
+
+				// If the internal model doesn't yet exist, this call will implicitly create it anyway.
+				// It will also initialise it, which in this case implies moving it into the correct location in internal space
+				// and populate it with crew, which is what we want.
+				part.SpawnCrew();
+
+				// Once that happens, the internal will have the correct location for viewing from IVA.
+				// So we make note of it.
+				originalParent = part.internalModel.transform.parent;
+				originalPosition = part.internalModel.transform.localPosition;
+				originalRotation = part.internalModel.transform.localRotation;
+
+				// And then we remember the root part and the active vessel these coordinates refer to.
+				knownRootPart = vessel.rootPart;
+				lastActiveVessel = FlightGlobals.ActiveVessel;
+
+				// And just in case, set shaders to opaque state...
+				SetShaders(false);
 			}
 		}
 
@@ -119,11 +167,9 @@ namespace JSI
 			if (HighLogic.LoadedSceneIsEditor)
 				return;
 
-			// If the internal does not exist, for example, because the vessel was switched, make sure it does by force-creating it.
-			if (part.internalModel == null) {
-				part.CreateInternalModel();
-				// Notice that spawning crew also causes the internal to go invisible...
-				part.SpawnCrew();
+			// If the root part changed, or the IVA is mysteriously missing, we reset it and take note of where it ended up.
+			if (vessel.rootPart != knownRootPart || lastActiveVessel != FlightGlobals.ActiveVessel || part.internalModel == null) {
+				ResetIVA();
 			}
 
 			// Now we need to make sure that the list of portraits in the GUI conforms to what actually is in the active vessel.
@@ -165,12 +211,7 @@ namespace JSI
 					SetCameraCullingMask("Camera 00", false);
 
 					// So once everything is hidden again, we undo the change in shaders to conceal the fact that you can't see other internals.
-					if (restoreShadersOnIVA & shadersAreTransparent) {
-						foreach (KeyValuePair<Transform,Shader> backup in shadersBackup) {
-							backup.Key.renderer.material.shader = backup.Value;
-						}
-						shadersAreTransparent = false;
-					}
+					SetShaders(false);
 
 				} else {
 					// Otherwise, we're out of IVA, so we can proceed with setting up the pods for exterior view.
@@ -181,12 +222,7 @@ namespace JSI
 					part.internalModel.SetVisible(true);
 
 					// And for a good measure we make sure the shader change has been applied.
-					if (restoreShadersOnIVA && !shadersAreTransparent) {
-						foreach (KeyValuePair<Transform,Shader> backup in shadersBackup) {
-							backup.Key.renderer.material.shader = transparentShader;
-						}
-						shadersAreTransparent = true;
-					}
+					SetShaders(true);
 
 					// Now we attach the restored IVA directly into the pod at zero local coordinates and rotate it,
 					// so that it shows up on the main outer view camera in the correct location.
