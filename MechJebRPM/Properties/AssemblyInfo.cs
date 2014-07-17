c069e1126003628650aa74ca70a3037f01079916
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-07-18 02:25:21 +0400

    Bumping MJ reference version.

--- MechJebRPM/Properties/AssemblyInfo.cs
+++ MechJebRPM/Properties/AssemblyInfo.cs
@@ -6,9 +6,10 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("MechJebRPM")]
 [assembly: AssemblyDescription("RasterPropMonitor / MechJeb2 interface plugin for Kerbal Space Program")]
 
-// For KSP purposes we are MechJebRPM version 0.17...
-[assembly: KSPAssembly("MechJebRPM", 0, 17)]
-// Depends on RPM 0.17 and MechJeb 2.2.
+// For KSP purposes we are MechJebRPM version 0.18, because
+// 0.17 depended on KSPAssembly version 2.2.
+[assembly: KSPAssembly("MechJebRPM", 0, 18)]
+// Depends on RPM 0.17 and MechJeb 2.3.
 // Supposedly these help avoid the problems of plugin loading order.
 [assembly: KSPAssemblyDependency("RasterPropMonitor", 0, 17)]
-[assembly: KSPAssemblyDependency("MechJeb2", 2, 2)]
+[assembly: KSPAssemblyDependency("MechJeb2", 2, 3)]
