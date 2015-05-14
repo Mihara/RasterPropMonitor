64912ecb2de5c171b0e78bbaf6a2b72e157bb90e
MOARdV <cwebb@babylonbycandlelight.com>
2015-05-14 08:43:56 -0500

    Add alternative construtor / evaluator
    Allows us to supply RPMC at call time, instead of embedding it in the
    object.

--- RasterPropMonitor/Core/VariableOrNumber.cs
+++ RasterPropMonitor/Core/VariableOrNumber.cs
@@ -1,40 +1,81 @@
 namespace JSI
 {
-	public class VariableOrNumber
-	{
-		private bool warningMade;
-		private readonly float? value;
-		private readonly string variableName;
-		private readonly object owner;
-		private readonly RasterPropMonitorComputer comp;
+    public class VariableOrNumber
+    {
+        private bool warningMade;
+        private readonly float? value;
+        private readonly string variableName;
+        private readonly object owner;
+        private readonly RasterPropMonitorComputer comp;
 
-		public VariableOrNumber(string input, RasterPropMonitorComputer compInstance, object caller)
-		{
-			owner = caller;
-			comp = compInstance;
-			float realValue;
-			if (float.TryParse(input, out realValue))
-				value = realValue;
-			else
-				variableName = input.Trim();
-		}
+        public VariableOrNumber(string input, RasterPropMonitorComputer compInstance, object caller)
+        {
+            owner = caller;
+            comp = compInstance;
+            float realValue;
+            if (float.TryParse(input, out realValue))
+            {
+                value = realValue;
+            }
+            else
+            {
+                variableName = input.Trim();
+            }
+        }
 
-		public bool Get(out float destination)
-		{
-			if (value != null) {
-				destination = value.Value;
-				return true;
-			}
-			destination = comp.ProcessVariable(variableName).MassageToFloat();
-			if (float.IsNaN(destination) || float.IsInfinity(destination)) {
-				if (!warningMade) {
-					JUtil.LogMessage(owner, "Warning, {0} can fail to produce a usable number.", variableName);
-					warningMade = true;
-					return false;
-				}
-			}
-			return true;
-		}
-	}
+        public VariableOrNumber(string input, object caller)
+        {
+            owner = caller;
+            comp = null;
+            float realValue;
+            if (float.TryParse(input, out realValue))
+            {
+                value = realValue;
+            }
+            else
+            {
+                variableName = input.Trim();
+            }
+        }
+
+        public bool Get(out float destination)
+        {
+            if (value != null)
+            {
+                destination = value.Value;
+                return true;
+            }
+            destination = comp.ProcessVariable(variableName).MassageToFloat();
+            if (float.IsNaN(destination) || float.IsInfinity(destination))
+            {
+                if (!warningMade)
+                {
+                    JUtil.LogMessage(owner, "Warning, {0} can fail to produce a usable number.", variableName);
+                    warningMade = true;
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool Get(out float destination, RasterPropMonitorComputer incomp)
+        {
+            if (value != null)
+            {
+                destination = value.Value;
+                return true;
+            }
+            destination = incomp.ProcessVariable(variableName).MassageToFloat();
+            if (float.IsNaN(destination) || float.IsInfinity(destination))
+            {
+                if (!warningMade)
+                {
+                    JUtil.LogMessage(owner, "Warning, {0} can fail to produce a usable number.", variableName);
+                    warningMade = true;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
-
