be6f5bb5e3bd69daf1075533efa9d84d83191fd7
MOARdV <cwebb@babylonbycandlelight.com>
2015-08-19 16:04:43 -0500

    Provide direct accessors to sideslip/AoA
    Instead of adding overhead for the HUD to query variables.  Issue #335

--- RasterPropMonitor/Handlers/JSIHeadsUpDisplay.cs
+++ RasterPropMonitor/Handlers/JSIHeadsUpDisplay.cs
@@ -287,7 +287,7 @@ namespace JSI
 
             if (progradeLadderIcon != null)
             {
-                float AoA = comp.ProcessVariable("PITCH", persistence).MassageToFloat() - comp.ProcessVariable("ANGLEOFATTACK", persistence).MassageToFloat();
+                float AoA = comp.AbsoluteAoA;
                 AoA = (float)JUtil.ClampDegrees180(AoA);
                 if (float.IsNaN(AoA))
                 {
@@ -341,7 +341,7 @@ namespace JSI
 
             if (progradeHeadingIcon != null)
             {
-                float slipAngle = comp.ProcessVariable("SIDESLIP", persistence).MassageToFloat();
+                float slipAngle = comp.Sideslip;
                 float slipTC = JUtil.DualLerp(0f, 1f, 0f, 360f, rotationVesselSurface.eulerAngles.y + slipAngle);
                 float slipIconX = JUtil.DualLerp(progradeHeadingIconOrigin - 0.5f * headingBarPosition.z, progradeHeadingIconOrigin + 0.5f * headingBarPosition.z, heading - headingBarTextureWidth, heading + headingBarTextureWidth, slipTC);
 
