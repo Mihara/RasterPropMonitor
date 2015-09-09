1c86d145e719de4658fc9b69511a035a0a3483f7
MOARdV <cwebb@babylonbycandlelight.com>
2015-09-09 15:38:51 -0500

    Refactor resource and mass tracking
    Maybe make it a little less costly.

--- RasterPropMonitor/Core/ResourceDataStorage.cs
+++ RasterPropMonitor/Core/ResourceDataStorage.cs
@@ -1,130 +1,244 @@
+/*****************************************************************************
+ * RasterPropMonitor
+ * =================
+ * Plugin for Kerbal Space Program
+ *
+ *  by Mihara (Eugene Medvedev), MOARdV, and other contributors
+ * 
+ * RasterPropMonitor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, revision
+ * date 29 June 2007, or (at your option) any later version.
+ * 
+ * RasterPropMonitor is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with RasterPropMonitor.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
 using System.Collections.Generic;
 using System;
 
 namespace JSI
 {
-	public class ResourceDataStorage
-	{
-		// Mihara: This is rather bad code. Maybe when I'm not feeling so stupid, I'll rewrite it.
-		private readonly Dictionary<string,int> nameID = new Dictionary<string, int>();
-		private readonly Dictionary<int,ResourceData> data = new Dictionary<int,ResourceData>();
-		private double lastcheck;
-		private const double secondsBetweenSamples = 0.5d;
+    public class ResourceDataStorage
+    {
+        private readonly ResourceData[] rs;
+        private double lastcheck;
+        private const double secondsBetweenSamples = 0.5d;
 
-		public ResourceDataStorage()
-		{
-			foreach (PartResourceDefinition thatResource in PartResourceLibrary.Instance.resourceDefinitions) {
-				data.Add(thatResource.id, new ResourceData());
-				data[thatResource.id].density = thatResource.density;
-				nameID.Add(thatResource.name, thatResource.id);
-			}
+        private class ResourceComparer : IComparer<ResourceData>
+        {
+            public int Compare(ResourceData a, ResourceData b)
+            {
+                return string.Compare(a.name, b.name);
+            }
+        }
 
-		}
+        public ResourceDataStorage()
+        {
+            int resourceCount = 0;
+            foreach (PartResourceDefinition thatResource in PartResourceLibrary.Instance.resourceDefinitions)
+            {
+                ++resourceCount;
+            }
 
-		public void StartLoop(double time)
-		{
+            rs = new ResourceData[resourceCount];
+            int index = 0;
+            foreach (PartResourceDefinition thatResource in PartResourceLibrary.Instance.resourceDefinitions)
+            {
+                rs[index] = new ResourceData();
+                rs[index].name = thatResource.name;
+                rs[index].density = thatResource.density;
+                ++index;
+            }
 
-			foreach (KeyValuePair<int, ResourceData> resource in data) {
-				if (time - lastcheck > secondsBetweenSamples) {
-					data[resource.Key].delta = (data[resource.Key].previous - data[resource.Key].current) / (time - lastcheck);
-					data[resource.Key].previous = data[resource.Key].current;
-				}
-				data[resource.Key].current = 0;
-				data[resource.Key].max = 0;
-				data[resource.Key].stage = 0;
-				data[resource.Key].stagemax = 0;
-				data[resource.Key].ispropellant = false;
-			}
+            // Alphabetize our list
+            Array.Sort(rs, new ResourceComparer());
+        }
 
-			if (time - lastcheck > secondsBetweenSamples) {
-				lastcheck = time;
-			}
-		}
+        public void StartLoop(double time)
+        {
+            bool updateDeltas = false;
+            float invDeltaT = 1.0f;
+            if (time - lastcheck > secondsBetweenSamples)
+            {
+                updateDeltas = true;
+                invDeltaT = (float)(1.0 / (time - lastcheck));
+            }
 
-		public void MarkPropellant(Propellant propel)
-		{
-			foreach (PartResource resource in propel.connectedResources) {
-				data[resource.info.id].ispropellant = true;
-			}
-		}
+            for (int i = 0; i < rs.Length; ++i)
+            {
+                if (updateDeltas)
+                {
+                    rs[i].delta = (rs[i].previous - rs[i].current) * invDeltaT;
+                    rs[i].previous = rs[i].current;
+                }
+                rs[i].current = 0.0f;
+                rs[i].max = 0.0f;
+                rs[i].stage = 0.0f;
+                rs[i].stagemax = 0.0f;
+                rs[i].ispropellant = false;
 
-		public string[] Alphabetic()
-		{
-			var names = new List<string>();
-			foreach (KeyValuePair<string,int> resource in nameID) {
-				if (data[resource.Value].max > 0) {
-					names.Add(resource.Key);
-				}
-			}
-			var result = names.ToArray();
-			Array.Sort(result);
-			return result;
-		}
+            }
 
-		public double PropellantMass(bool stage)
-		{
-			double mass = 0;
-			foreach (KeyValuePair<int,ResourceData> resource in data) {
-				if (resource.Value.ispropellant) {
-					if (stage)
-						mass += resource.Value.density * resource.Value.stage;
-					else
-						mass += resource.Value.density * resource.Value.current;
-				}
-			}
-			return mass;
-		}
+            if (updateDeltas)
+            {
+                lastcheck = time;
+            }
+        }
 
-		public object ListElement(string resourceName, string valueType, bool stage)
-		{
-			if (!nameID.ContainsKey(resourceName))
-				return 0d;
-			switch (valueType) {
-				case "":
-				case "VAL":
-					return stage ? data[nameID[resourceName]].stage : data[nameID[resourceName]].current;
-				case "DENSITY":
-					return data[nameID[resourceName]].density;
-				case "DELTA":
-					return data[nameID[resourceName]].delta;
-				case "DELTAINV":
-					return -data[nameID[resourceName]].delta;
-				case "MASS":
-					return data[nameID[resourceName]].density * (stage ? data[nameID[resourceName]].stage : data[nameID[resourceName]].current);
-				case "MAXMASS":
-					return data[nameID[resourceName]].density * (stage ? data[nameID[resourceName]].stagemax : data[nameID[resourceName]].max);
-				case "MAX":
-					return stage ? data[nameID[resourceName]].stagemax : data[nameID[resourceName]].max;
-				case "PERCENT":
-					if (stage)
-						return data[nameID[resourceName]].stagemax > 0 ? data[nameID[resourceName]].stage / data[nameID[resourceName]].stagemax : 0d;
-					return  data[nameID[resourceName]].max > 0 ? data[nameID[resourceName]].current / data[nameID[resourceName]].max : 0d;
-			}
-			return 0d;
-		}
+        public void MarkPropellant(Propellant propel)
+        {
+            foreach (PartResource resource in propel.connectedResources)
+            {
+                try
+                {
+                    ResourceData r = Array.Find(rs, t => t.name == resource.info.name);
+                    r.ispropellant = true;
+                }
+                catch(Exception e)
+                {
+                    JUtil.LogErrorMessage(this, "Error in MarkPropellant({0}): {1}", resource.info.name,e);
+                }
+            }
+        }
 
-		public void Add(PartResource resource)
-		{
-			data[resource.info.id].current += resource.amount;
-			data[resource.info.id].max += resource.maxAmount;
-		}
+        public void GetActiveResourceNames(ref string[] result)
+        {
+            int currentIndex = 0;
+            int currentLength = (result == null) ? 0 : result.Length;
+            for (int i = 0; i < rs.Length; ++i)
+            {
+                if (rs[i].max > 0.0)
+                {
+                    if (currentIndex == currentLength)
+                    {
+                        ++currentLength;
+                        Array.Resize(ref result, currentLength);
+                    }
+                    if (result[currentIndex] != rs[i].name)
+                    {
+                        result[currentIndex] = rs[i].name;
+                    }
+                    ++currentIndex;
+                }
+            }
 
-		public void SetActive(Vessel.ActiveResource resource)
-		{
-			data[resource.info.id].stage = resource.amount;
-			data[resource.info.id].stagemax = resource.maxAmount;
-		}
+            if(currentIndex > 0 && result.Length > currentIndex)
+            {
+                Array.Resize(ref result, currentIndex);
+            }
+        }
 
-		private class ResourceData
-		{
-			public double current;
-			public double max;
-			public double previous;
-			public double stage;
-			public double stagemax;
-			public double density;
-			public double delta;
-			public bool ispropellant;
-		}
-	}
+        public double PropellantMass(bool stage)
+        {
+            double mass = 0.0;
+            for (int i = 0; i < rs.Length; ++i)
+            {
+                if (rs[i].ispropellant)
+                {
+                    mass += rs[i].density * ((stage) ? rs[i].stage : rs[i].current);
+                }
+            }
+            return mass;
+        }
+
+        public object ListElement(string resourceName, string valueType, bool stage)
+        {
+            double v = 0.0;
+
+            try
+            {
+                ResourceData resource = Array.Find(rs, t => t.name == resourceName);
+
+                switch (valueType)
+                {
+                    case "":
+                    case "VAL":
+                        v = stage ? resource.stage : resource.current;
+                        break;
+                    case "DENSITY":
+                        v = resource.density;
+                        break;
+                    case "DELTA":
+                        v = resource.delta;
+                        break;
+                    case "DELTAINV":
+                        v = -resource.delta;
+                        break;
+                    case "MASS":
+                        v = resource.density * (stage ? resource.stage : resource.current);
+                        break;
+                    case "MAXMASS":
+                        v = resource.density * (stage ? resource.stagemax : resource.max);
+                        break;
+                    case "MAX":
+                        v = stage ? resource.stagemax : resource.max;
+                        break;
+                    case "PERCENT":
+                        if (stage)
+                        {
+                            v = resource.stagemax > 0 ? resource.stage / resource.stagemax : 0d;
+                        }
+                        else
+                        {
+                            v = resource.max > 0 ? resource.current / resource.max : 0d;
+                        }
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                JUtil.LogErrorMessage(this, "Error finding {0}-{2}: {1}", resourceName, e, valueType);
+            }
+
+            return v;
+        }
+
+        public void Add(PartResource resource)
+        {
+            try
+            {
+                ResourceData res = Array.Find(rs, t => t.name == resource.info.name);
+                res.current += (float)resource.amount;
+                res.max += (float)resource.maxAmount;
+            }
+            catch (Exception e)
+            {
+                JUtil.LogErrorMessage(this, "Error adding {0}: {1}", resource.info.name, e);
+            }
+        }
+
+        public void SetActive(Vessel.ActiveResource resource)
+        {
+            try
+            {
+                ResourceData res = Array.Find(rs, t => t.name == resource.info.name);
+                res.stage = (float)resource.amount;
+                res.stagemax = (float)resource.maxAmount;
+            }
+            catch (Exception e)
+            {
+                JUtil.LogErrorMessage(this, "Error SetActive {0}: {1}", resource.info.name, e);
+            }
+        }
+
+        private class ResourceData
+        {
+            public string name;
+
+            public float current;
+            public float max;
+            public float previous;
+            public float stage;
+            public float stagemax;
+            public float density;
+            public float delta;
+
+            public bool ispropellant;
+        }
+    }
 }
