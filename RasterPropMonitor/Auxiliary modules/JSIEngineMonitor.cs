b52cbc3d81e80e82c6ae5cc9e570f152b6072b37
MOARdV <cwebb@babylonbycandlelight.com>
2015-11-24 08:45:19 -0600

    JSIEngine phase one
    JSIEngineMonitor, plus MultiModeEngine toggle.  Issue #430

---
+++ RasterPropMonitor/Auxiliary modules/JSIEngineMonitor.cs
@@ -0,0 +1,166 @@
+﻿/*****************************************************************************
+ * RasterPropMonitor
+ * =================
+ * Plugin for Kerbal Space Program
+ *
+ *  by Mihara (Eugene Medvedev), MOARdV, and other contributors
+ * 
+ * RasterPropMonitor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, revision
+ * date 29 June 2007, or (at your option) any later version.
+ * 
+ * RasterPropMonitor is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with RasterPropMonitor.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+using System;
+using System.Collections.Generic;
+
+namespace JSI
+{
+    /// <summary>
+    /// Provides a module for RasterPropMonitor to use for providing granular
+    /// control of an engine as well as detailed information.
+    /// </summary>
+    class JSIEngineMonitor : PartModule
+    {
+        // Internal data storage.
+        [KSPField(isPersistant = true)]
+        public int enginedID = 0;
+
+        // Fields to handle right-click GUI.
+        [KSPField(guiActive = true, guiActiveEditor = true, guiName = "Engine ID: ")]
+        public string engineDisplayName;
+
+        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "ID +")]
+        public void IdPlus()
+        {
+            enginedID++;
+            UpdateName();
+        }
+
+        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "ID -")]
+        public void IdMinus()
+        {
+            enginedID--;
+            if (enginedID <= 0)
+                enginedID = 0;
+            UpdateName();
+        }
+
+        private void UpdateName()
+        {
+            engineDisplayName = (enginedID > 0) ? enginedID.ToString() : "Untracked";
+        }
+
+        private int mmeIndex = -1;
+        private int engineMode1Index = -1;
+        private int engineMode2Index = -1;
+
+        public void Start()
+        {
+            if (!HighLogic.LoadedSceneIsFlight)
+            {
+                return;
+            }
+
+            for(int i=0; i<part.Modules.Count; ++i)
+            {
+                if(part.Modules[i] is MultiModeEngine)
+                {
+                    if (mmeIndex == -1)
+                    {
+                        mmeIndex = i;
+                        JUtil.LogMessage(this, "Start(): mmeIndex = {0}", i);
+                    }
+                    else
+                    {
+                        JUtil.LogErrorMessage(this, "Found more than one MultiModeEngine on {0} - I don't know what to do with it.", part.name);
+                    }
+                }
+                else if(part.Modules[i] is ModuleEngines)
+                {
+                    if(engineMode1Index == -1)
+                    {
+                        engineMode1Index = i;
+                        JUtil.LogMessage(this, "Start(): engineMode1Index = {0}", i);
+                    }
+                    else if(engineMode2Index == -1)
+                    {
+                        engineMode2Index = i;
+                        JUtil.LogMessage(this, "Start(): engineMode2Index = {0}", i);
+                    }
+                    else
+                    {
+                        JUtil.LogErrorMessage(this, "Found more than 2 ModuleEngines on {0} - I don't know what to do with them.", part.name);
+                    }
+                }
+            }
+
+            if(engineMode1Index == -1)
+            {
+                Destroy(this);
+                // No engines!
+            }
+            UpdateName();
+        }
+
+        public void FixedUpdate()
+        {
+            if (HighLogic.LoadedSceneIsEditor)
+            {
+                return;
+            }
+
+            if (!JUtil.IsActiveVessel(vessel))
+            {
+                return;
+            }
+        }
+
+        public bool GetRunningPrimary()
+        {
+            if(mmeIndex == -1)
+            {
+                return true;
+            }
+            else
+            {
+                MultiModeEngine mme = part.Modules[mmeIndex] as MultiModeEngine;
+                return mme.runningPrimary;
+            }
+        }
+
+        public void SetRunningPrimary(bool state)
+        {
+            try
+            {
+                if (mmeIndex > -1)
+                {
+                    MultiModeEngine mme = part.Modules[mmeIndex] as MultiModeEngine;
+
+                    // There doesn't appear to be a simpler way to switch modes.
+                    // One this I could do is store the even index instead of looping
+                    // over it every time we change modes.
+                    if (mme.runningPrimary != state)
+                    {
+                        var ev = mme.Events["ModeEvent"];
+                        if (ev != null)
+                        {
+                            ev.Invoke();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                // no-op
+            }
+        }
+    }
+}
--- RasterPropMonitor/Handlers/JSIEngine.cs
+++ RasterPropMonitor/Handlers/JSIEngine.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*****************************************************************************
+ * RasterPropMonitor
+ * =================
+ * Plugin for Kerbal Space Program
+ *
+ *  by Mihara (Eugene Medvedev), MOARdV, and other contributors
+ * 
+ * RasterPropMonitor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, revision
+ * date 29 June 2007, or (at your option) any later version.
+ * 
+ * RasterPropMonitor is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with RasterPropMonitor.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,22 +27,46 @@ namespace JSI
 {
     public class JSIEngine:IJSIModule
     {
-        //else if (pm is MultiModeEngine)
-        //{
-        //    var thatMME = pm as MultiModeEngine;
-        //    JUtil.LogMessage(this, "Set running primary? Is {0}, should be {1}; mode {2}", thatMME.runningPrimary, !vessel.ActionGroups.groups[actionGroupID[0]], thatMME.mode);
-        //    var events = thatMME.Events;
-        //    foreach (var ev in events)
-        //    {
-        //        JUtil.LogMessage(this, "... event {0} / {1}", ev.name, ev.guiName);
-        //        if(ev.name == "ModeEvent")
-        //        {
-        //            if(thatMME.runningPrimary != vessel.ActionGroups.groups[actionGroupID[0]])
-        //            {
-        //                ev.Invoke();
-        //            }
-        //        }
-        //    }
-        //}
+        public JSIEngine()
+        {
+
+        }
+
+        public bool GetRunningPrimary()
+        {
+            // TODO: select which engine
+            bool runningPrimary = true;
+            foreach (JSIEngineMonitor mon in FindEngineMonitorIn(vessel))
+            {
+                if(!mon.GetRunningPrimary())
+                {
+                    runningPrimary = false;
+                }
+            }
+
+            return runningPrimary;
+        }
+
+        public void SetRunningPrimary(bool state)
+        {
+            foreach(JSIEngineMonitor mon in FindEngineMonitorIn(vessel))
+            {
+                mon.SetRunningPrimary(state);
+            }
+        }
+
+        private static IEnumerable<JSIEngineMonitor> FindEngineMonitorIn(Vessel vessel)
+        {
+            for (int i = 0; i < vessel.Parts.Count; ++i )
+            {
+                for (int j = 0; j < vessel.Parts[i].Modules.Count; ++j )
+                {
+                    if (vessel.Parts[i].Modules[j] is JSIEngineMonitor)
+                    {
+                        yield return vessel.Parts[i].Modules[j] as JSIEngineMonitor;
+                    }
+                }
+            }
+        }
     }
 }
