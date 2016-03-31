baa5cfe4cb08d9e9a4004b5354c5db536ad8c438
MOARdV <cwebb@babylonbycandlelight.com>
2016-03-31 13:56:28 -0500

    Bump .version back to 0.24.2
    That shouldn't be posted with a new version until I'm ready to release.
    
    Also update the copyright into in SharedAssemblyInfo

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct ("RasterPropMonitor")]
-[assembly: AssemblyCopyright ("Copyright ©2013-2015 by Mihara, MOARdV, and other contributors, released under the terms of GNU GPLv3")]
+[assembly: AssemblyCopyright ("Copyright ©2013-2016 by Mihara, MOARdV, and other contributors, released under the terms of GNU GPLv3")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // Now this is the actual version number with build number.
 // As I release newer ones, I'll bump them manually.
 // Build number is altered automatically.
-[assembly: AssemblyFileVersion("0.24.2")]
+[assembly: AssemblyFileVersion("0.25.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
