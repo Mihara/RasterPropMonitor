12cc54c66928fe72b2f543854248c17eb32738d8
MOARdV <cwebb@babylonbycandlelight.com>
2016-05-07 07:57:31 -0500

    Add a field to offset the HUD ladder
    Issue #418.

--- RasterPropMonitor/Handlers/JSIHeadsUpDisplay.cs
+++ RasterPropMonitor/Handlers/JSIHeadsUpDisplay.cs
@@ -58,6 +58,8 @@ namespace JSI
         public bool use360horizon = true;
         [KSPField] // Number of texels of the horizon texture to draw (width).
         public Vector2 horizonTextureSize = new Vector2(1f, 1f);
+        [KSPField]
+        public Vector2 horizonOffset = Vector2.zero;
 
         [KSPField]
         public string headingBar = string.Empty;
@@ -143,7 +145,7 @@ namespace JSI
                 Material ladderMaterial = new Material(ladderShader);
 
                 // _CropBound is in device normalized coordinates (-1 - +1)
-                Vector4 cropBound = new Vector4(-horizonSize.x / screenWidth, -horizonSize.y / screenHeight, horizonSize.x / screenWidth, horizonSize.y / screenHeight);
+                Vector4 cropBound = new Vector4((horizonOffset.x - horizonSize.x) / screenWidth, (horizonOffset.y - horizonSize.y) / screenHeight, (horizonOffset.x + horizonSize.x) / screenWidth, (horizonOffset.y + horizonSize.y) / screenHeight);
                 ladderMaterial.SetVector("_CropBound", cropBound);
                 ladderMaterial.color = Color.white;
                 ladderMaterial.mainTexture = GameDatabase.Instance.GetTexture(horizonTexture.EnforceSlashes(), false);
@@ -157,7 +159,7 @@ namespace JSI
                     ladderMaterial.mainTexture.wrapMode = TextureWrapMode.Clamp;
 
                     ladderMesh = JUtil.CreateSimplePlane("JSIHeadsUpDisplayLadder" + hudCamera.GetInstanceID(), horizonDrawSize, new Rect(0.0f, 0.0f, 1.0f, 1.0f), drawingLayer);
-                    ladderMesh.transform.position = new Vector3(0, 0, 1.45f);
+                    ladderMesh.transform.position = new Vector3(horizonOffset.x, horizonOffset.y, 1.45f);
                     ladderMesh.GetComponent<Renderer>().material = ladderMaterial;
                     ladderMesh.transform.parent = cameraBody.transform;
 
