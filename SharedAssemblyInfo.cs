67234a7cb2e7a3f5f49846f22265b575f6fb1f62
MOARdV <cwebb@babylonbycandlelight.com>
2016-06-22 09:00:31 -0500

    Bump to version v0.27.0
    1.1.3 is not backwards compatible to 1.1.2 for RPM.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 // Revision number is altered automatically.
-[assembly: AssemblyVersion("0.26.1.*")]
+[assembly: AssemblyVersion("0.27.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
