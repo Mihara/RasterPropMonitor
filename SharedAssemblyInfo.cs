f82786ee542276fc4adb98bf571faffd7692af0a
MOARdV <cwebb@babylonbycandlelight.com>
2018-01-01 08:30:02 -0600

    Allow MJ interface to work without spaceplane
    autopilot.  AP was substantially changed in MJ dev build 742.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct ("RasterPropMonitor")]
-[assembly: AssemblyCopyright ("Copyright ©2013-2017 by Mihara, MOARdV, and other contributors, released under the terms of GNU GPLv3")]
+[assembly: AssemblyCopyright ("Copyright ©2013-2018 by Mihara, MOARdV, and other contributors, released under the terms of GNU GPLv3")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -12,7 +12,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 // Revision number is altered automatically.
-[assembly: AssemblyVersion("0.29.2.*")]
+[assembly: AssemblyVersion("0.29.3.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
